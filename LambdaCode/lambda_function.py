fd8a561fd61a9675738542d6204f8d3a5a801528
deveshharjicci <devesh.harji@creativecapsule.com>
2019-11-14 17:20:23 +0530

    lambda updated

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -3,5 +3,5 @@ import json
 
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
+    response = ec2.describe_addresses()
     return {"statusCode": 200, "body": json.dumps(response)}
